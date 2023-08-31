16eadf7afa2c937deeb79208803c5eff6b40d21d
wimonmani <137855706+wimonmani@users.noreply.github.com>
2023-08-31 23:33:23 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,58 @@
+var number1 = document.getElementById('number1');
+var number2 = document.getElementById('number2');
+var mark = document.getElementById('mark');
+var answer = document.getElementById('answer');
+var result = document.getElementById('result');
+var score = document.getElementById('score');
+
+var num1, num2, sum, score = 0, point = 0;
+score.innerHTML = score + "/" + point;
+
+function random() {
+    num1 = Math.floor(Math.random() * 11);
+    num2 = Math.floor(Math.random() * 11);
+    number1.value = num1;
+    number2.value = num2;
+    answer.value = '';
+}
+
+function start() {
+    point = 0;
+    score = 0;
+    score.innerHTML = score + "/" + point;
+    random();
+}
+
+function selectmark(value) {
+    mark.innerHTML = value;
+}
+
+function markcheck() {
+    if (mark.innerHTML == "+") {
+        sum = num1 + num2;
+    } else if (mark.innerHTML == "-") {
+        sum = num1 - num2;
+    } else if (mark.innerHTML == "*") {
+        sum = num1 * num2;
+    } else if (mark.innerHTML == "÷") {
+        sum = num1 / num2;
+    }
+}
+
+function response() {
+    markcheck();
+    console.log('sum : ' + sum);
+    if (sum == answer.value) {
+        result.innerHTML = 'ถูกต้อง';
+        result.style.color = 'green';
+        score += 1;
+    } else {
+        result.innerHTML = 'ไม่ถูกต้อง!';
+        result.style.color = 'red';
+    }
+    point += 1;
+    score.innerHTML = score + "/" + point;
+    random();
+}
+
+random();
